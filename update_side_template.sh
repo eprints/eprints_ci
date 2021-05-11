1d51116a5ef07c4d16dc6ccc61622214fa8babc1
David R Newman <drn@ecs.soton.ac.uk>
2021-05-11 22:59:28 +0000

    Adds additional scripts for managing CI.  Updates side file for public page tests.  Adds extra config files.  Reorganises directory structure to manage templates.

--- update_side_template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SIDE_FILEPATH=$1
-SIDE_FILENAME=`basename $SIDE_FILEPATH`
-DIR=`dirname $0`
-cd $DIR
-cat $SIDE_FILEPATH | sed 's!"url": "[^"]\+",!"url": "EPRINTS_URL",!' | sed 's!"urls": \["[^"]\+"\],!"urls": ["EPRINTS_URL"],!' > $DIR/side_templates/${SIDE_FILENAME}.tmpl
